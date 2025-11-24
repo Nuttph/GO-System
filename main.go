7945d0346a64df32fb1f6e11cc9954b6dc4c2048
Nuttaphon <nathn2420@gmail.com>
2025-11-24 21:12:12 +0700

    getting book

--- main.go
+++ main.go
@@ -8,11 +8,26 @@ type Message struct {
 	Message string `json:"message"`
 }
 
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var book []Book
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/", message)
+	book = append(book, Book{ID: 1, Title: "Book 1", Author: "Author 1"})
+	book = append(book, Book{ID: 2, Title: "Book 2", Author: "Author 2"})
+	book = append(book, Book{ID: 3, Title: "Book 3", Author: "Author 3"})
+	book = append(book, Book{ID: 4, Title: "Book 4", Author: "Author 4"})
+	book = append(book, Book{ID: 5, Title: "Book 5", Author: "Author 5"})
 
+	app.Get("/", message)
+	app.Get("/books", getBooks)
+	app.Get("/books/:id", getBook)
 	app.Listen(":8000")
 }
 
@@ -21,4 +36,25 @@ func message(c *fiber.Ctx) error {
 		Message: "Hello world!",
 	}
 	return c.JSON(message)
+}
+
+func getBooks(c *fiber.Ctx) error {
+	return c.JSON(book)
+}
+
+func getBook(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	if err != nil {
+		return c.Status(400).SendString("Invalid ID")
+	}
+
+	for _, b := range book {
+		if b.ID == id {
+			return c.JSON(b)
+		}
+	}
+
+	return c.Status(404).SendString("Book not found")
+
 }
